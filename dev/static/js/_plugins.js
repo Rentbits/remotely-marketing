24ef7a278b458ab947d1831b11579d1986c5401a
Gustavo Esquinca <g.esq.ca@gmail.com>
2014-03-13 13:41:27 -0600

    Added Headroom jQuery script and cleaned up main js file

--- dev/static/js/_plugins.js
+++ dev/static/js/_plugins.js
@@ -3,3 +3,4 @@
 // @codekit-prepend "lib/swipe.js"
 // @codekit-prepend "lib/verge.js"
 // @codekit-prepend "lib/headroom.js"
+// @codekit-prepend "lib/jQuery.headroom.js"
--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -3,148 +3,131 @@
 
 (function(window, document, $) { // put the vars you need and match them at the bottom
 
-    // All 4 vars are localized in here - makes them read faster b/c they are in local scope
-    // $ works as alias for jQuery in here - ensures no conflict with other libs
-    // R works as alias for Response in here - R.band(320) instead of Response.band(320)
-    // window and document minify down to single letter vars
+  // All 4 vars are localized in here - makes them read faster b/c they are in local scope
+  // $ works as alias for jQuery in here - ensures no conflict with other libs
+  // R works as alias for Response in here - R.band(320) instead of Response.band(320)
+  // window and document minify down to single letter vars
 
-    $.extend(verge);
+  // extend Verge
+  $.extend(verge);
 
-    // grab an element
-    var myElement = document.querySelector("nav");
-    // construct an instance of Headroom, passing the element
-    var headroom  = new Headroom(myElement);
-    // initialise
-    headroom.init();
+  // init headroom
+  $('#nav').headroom();
 
-    var $window = $(window);
-    var list = $('#showcase-list').children('li');
-    var activeListItem = list.first();
+  var $window = $(window);
+  var list = $('#showcase-list').children('li');
+  var activeListItem = list.first();
 
-    var page = {
-      cycleSlider : function (argument) {
-        // body...
-        var slider = $('#slider');
+  var page = {
+    cycleSlider : function (argument) {
+      // body...
+      var slider = $('#slider');
 
-        if ( $.inY(slider) === true ) {
-          window.mySwipe = new Swipe(slider[0], {
-            startSlide: 0,
-            auto: 4000,
-            transitionEnd: function(index, elem) {
-              page.showAside(index);
-            }
-          });
-          console.log('cycling');
-          $window.off('scroll');
-        }
-
-      },
-      showcase: function (argument) {
-        // body...
-        console.log('showcase func operational');
-
-        // var list = $('#showcase-list').children('li');
-
-        list.each(function(index, el) {
-          var $this = $(this);
-          $this.on('mouseenter', function() {
-            // event.preventDefault();
-            console.log(index);
-
-            window.mySwipe.slide(index, 400);
-
-            activeListItem = $this;
-            list.removeClass('active');
-            $this.addClass('active');
-          });
-        });
-
-      },
-      showAside: function (current) {
-        // body...
-        console.log(current);
-
-        var currentScreen = current;
-        var asideGroup = $('.showcase-aside ul');
-
-        asideGroup.each(function() {
-          var $this = $(this);
-          // var match = x;
-          if (currentScreen === $this.data('screen')) {
-            $this.addClass('show');
-          } else {
-            $this.removeClass('show');
+      if ( $.inY(slider) === true ) {
+        window.mySwipe = new Swipe(slider[0], {
+          startSlide: 0,
+          auto: 4000,
+          transitionEnd: function(index, elem) {
+            page.showAside(index);
           }
         });
-
-        activeListItem = list.eq(current);
-        list.removeClass('active');
-        activeListItem.addClass('active');
+        // console.log('cycling');
+        $window.off('scroll');
       }
-    };
-    // ====
 
-    window.mySwipe = new Swipe(document.getElementById('slider'), {
-      startSlide: 0,
-      speed: 400,
-      // auto: 3000,
-      continuous: true,
-      disableScroll: false,
-      stopPropagation: false,
-      callback: page.showcase(),
-    });
+    },
+    showcase: function (argument) {
+      // body...
+      // console.log('showcase func operational');
 
-    window.mySwipe = new Swipe(document.getElementById('slider-2'), {
-      startSlide: 0,
-      speed: 400,
-      auto: 3000,
-      continuous: true,
-      disableScroll: false,
-      stopPropagation: false,
-      callback: function(pos) {
+      list.each(function(index, el) {
+        var $this = $(this);
+        $this.on('mouseenter', function() {
+          // event.preventDefault();
+          // console.log(index);
 
-        var i = bullets.length;
-        while (i--) {
-          bullets[i].className = ' ';
+          window.mySwipe.slide(index, 400);
+
+          activeListItem = $this;
+          list.removeClass('active');
+          $this.addClass('active');
+        });
+      });
+
+    },
+    showAside: function (current) {
+      // body...
+      console.log(current);
+
+      var currentScreen = current;
+      var asideGroup = $('.showcase-aside ul');
+
+      asideGroup.each(function() {
+        var $this = $(this);
+        // var match = x;
+        if (currentScreen === $this.data('screen')) {
+          $this.addClass('show');
+        } else {
+          $this.removeClass('show');
         }
-        bullets[pos].className = 'on';
+      });
 
+      activeListItem = list.eq(current);
+      list.removeClass('active');
+      activeListItem.addClass('active');
+    }
+  };
+  // ====
+
+  window.mySwipe = new Swipe(document.getElementById('slider'), {
+    startSlide: 0,
+    speed: 400,
+    // auto: 3000,
+    continuous: true,
+    disableScroll: false,
+    stopPropagation: false,
+    callback: page.showcase(),
+  });
+
+  window.mySwipe = new Swipe(document.getElementById('slider-2'), {
+    startSlide: 0,
+    speed: 400,
+    auto: 3000,
+    continuous: true,
+    disableScroll: false,
+    stopPropagation: false,
+    callback: function(pos) {
+
+      var i = bullets.length;
+      while (i--) {
+        bullets[i].className = ' ';
       }
-    });
+      bullets[pos].className = 'on';
 
-    var bullets = document.getElementById('position').getElementsByTagName('li');
+    }
+  });
 
+  var bullets = document.getElementById('position').getElementsByTagName('li');
 
-    $window.on('scroll load', function(event) {
-      // event.preventDefault();
-      /* Act on the event */
-      page.cycleSlider();
+  $window.on('scroll load', function(event) {
+    // event.preventDefault();
+    /* Act on the event */
+    page.cycleSlider();
 
-    });
+  });
 
-    $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 800);
-          return false;
-        }
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 800);
+        return false;
       }
-    });
+    }
+  });
 
-    // var formSuccessFunc = function (argument) {
-    //   // body...
-    //   var target = $('.call-to-action');
-
-    //   $('.thank-you-message').addClass('show');
-    //   $('html,body').animate({
-    //     scrollTop: target.offset().top
-    //   }, 300);
-    // };
-
-    // site.showcase();
 
 }(this, this.document, this.jQuery)); // in global scope, this === window
